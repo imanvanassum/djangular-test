fbdebee668a1835690f402951a8663f2a301a71a
Iman van Assum <iman@vanassum.net>
2017-10-23 23:14:41 +0200

    Put the routes in their own TS file

--- authtest/src/app/app.module.ts
+++ authtest/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { StatusComponent } from './components/status/status.component';
-
-import { AuthService } from './services/auth.service';
-import { EnsureAuthenticated } from './services/ensure-authenticated.service';
-import { LoginRedirect } from './services/login-redirect.service';
-
 
 @NgModule({
   declarations: [
@@ -26,17 +22,9 @@ import { LoginRedirect } from './services/login-redirect.service';
     BrowserModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: 'login', component: LoginComponent, canActivate: [LoginRedirect] },
-      { path: 'status', component: StatusComponent, canActivate: [EnsureAuthenticated] },
-      { path: 'logout', component: LogoutComponent, canActivate: [EnsureAuthenticated] }
-    ])
+    AppRoutingModule
   ],
-  providers: [
-    AuthService,
-    LoginRedirect,
-    EnsureAuthenticated
-  ],
+  providers: [  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
